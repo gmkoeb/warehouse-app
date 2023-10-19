93e85abe3ff00624d9367317a0e6f5c9bc9d3a99
Gabriel Manika Koeb <gmkoeb@gmail.com>
2023-10-19 11:08:18 -0300

    Add columns address, cep and description to warehouses table

---
+++ spec/system/user_view_warehouse_details_spec.rb
@@ -0,0 +1,22 @@
+require 'rails_helper'
+
+describe 'User sees details of a warehouse' do
+  it 'and sees aditional info' do
+    # Arrange
+    w = Warehouse.new(name: 'Aeroporto SP', code: 'GRU', city: 'Guarulhos', area: 100_000,
+                      address: 'Avenida do Aeroporto, 1000', cep: '15000-000', 
+                      description: 'Galpão destinado para cargas internacionais.')
+    w.save()
+    # Act
+    visit('/')
+    click_on('Aeroporto SP')
+
+    # Assert
+    expect(page).to have_content('Galpão GRU')
+    expect(page).to have_content('Nome: Aeroporto SP')
+    expect(page).to have_content('Cidade: Guarulhos')
+    expect(page).to have_content('Área: 100000 m²')
+    expect(page).to have_content('Endereço: Avenida do Aeroporto, 1000. CEP: 15000-000')
+    expect(page).to have_content('Galpão destinado para cargas internacionais.')
+  end
+end
