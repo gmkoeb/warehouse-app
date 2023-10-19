8e931035e093bf59afc22b3157d4ac7d966c2f47
Gabriel Manika Koeb <gmkoeb@gmail.com>
2023-10-19 10:06:41 -0300

    Create Warehouse model and ran pending migrations.
    Also created tests to see warehouses in homepage

--- spec/system/user_view_homepage_spec.rb
+++ spec/system/user_view_homepage_spec.rb
@@ -10,4 +10,25 @@ describe 'User visits homepage' do
     # Assert
     expect(page).to have_content('Galpões & Estoque')
   end
+
+  it 'and sees registered warehouses' do
+    # Arrange
+    Warehouse.create(name: 'Rio', code: 'SDU', city: 'Rio de Janeiro', area:60_000)
+    Warehouse.create(name: 'Maceio', code: 'MCZ', city: 'Maceio', area:50_000)
+
+    # Act
+    visit('/')
+
+    # Assert
+    expect(page).to have_content('Rio')
+    expect(page).to have_content('Código: SDU')
+    expect(page).to have_content('Cidade: Rio de Janeiro')
+    expect(page).to have_content('60.000 m²')
+
+    expect(page).to have_content('Maceio')
+    expect(page).to have_content('Código: MCZ')
+    expect(page).to have_content('Cidade: Maceio')
+    expect(page).to have_content('50.000 m²')
+
+  end
 end
