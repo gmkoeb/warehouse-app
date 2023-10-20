1d36fc6a2ae00466d098298a23260534c7829ca5
Gabriel Manika Koeb <gmkoeb@gmail.com>
2023-10-20 18:13:01 -0300

    Include warehouse register funcitonality

--- app/controllers/warehouses_controller.rb
+++ app/controllers/warehouses_controller.rb
@@ -2,4 +2,19 @@ class WarehousesController < ApplicationController
   def show
     @warehouse = Warehouse.find(params[:id])
   end
+
+  def new
+  end
+
+  def create
+    warehouse_params = params.require(:warehouse).permit(:name, :code, :city, 
+                                                        :description, :address, :cep, :area )
+                                                        # Strong parameters
+
+    new_warehouse = Warehouse.new(warehouse_params)
+
+    new_warehouse.save()
+
+    redirect_to root_path
+  end
 end
