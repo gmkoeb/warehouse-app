800f252b8da5456c171e90fb7d59347f567de16f
Gabriel Manika Koeb <gmkoeb@gmail.com>
2023-10-26 14:50:56 -0300

    Change uniqueness validation to brand_name instead of corporate_name Include tests for Supplier model

--- app/models/supplier.rb
+++ app/models/supplier.rb
@@ -5,7 +5,8 @@ class Supplier < ApplicationRecord
   def should_generate_new_friendly_id?
     brand_name_changed?
   end
-  
+
   validates :corporate_name, :brand_name, :registration_number, :full_address, :city, :state, :email, presence: true
-  validates :corporate_name, :registration_number, uniqueness: true
+
+  validates :brand_name, :registration_number, uniqueness: true
 end
--- spec/models/supplier_spec.rb
+++ spec/models/supplier_spec.rb
@@ -1,4 +1,109 @@
 require 'rails_helper'
 
 RSpec.describe Supplier, type: :model do
+  describe 'valid?' do
+    it 'returns true when all fields are filled' do
+      # Arrange
+      new_supplier = Supplier.new(corporate_name: 'Razer Eletronics', brand_name: 'Razer', registration_number: '131341', 
+                                  full_address: 'Wall Street, 341', city: 'New York', state: 'NY', email: 'razer@gmail.com')
+      # Act
+      result = new_supplier.valid?
+      # Assert
+      expect(result).to eq(true)
+    end
+
+    it 'returns false when corporate_name is empty' do
+      # Arrange
+      new_supplier = Supplier.new(corporate_name: '', brand_name: 'Razer', registration_number: '131341', 
+                                  full_address: 'Wall Street, 341', city: 'New York', state: 'NY', email: 'razer@gmail.com')
+      # Act
+      result = new_supplier.valid?
+      # Assert
+      expect(result).to eq(false)
+    end
+
+    it 'returns false when brand_name is empty' do
+      # Arrange
+      new_supplier = Supplier.new(corporate_name: 'Razer Eletronics', brand_name: '', registration_number: '131341', 
+                                  full_address: 'Wall Street, 341', city: 'New York', state: 'NY', email: 'razer@gmail.com')
+      # Act
+      result = new_supplier.valid?
+      # Assert
+      expect(result).to eq(false)
+    end
+
+    it 'returns false when registration_number is empty' do
+      # Arrange
+      new_supplier = Supplier.new(corporate_name: 'Razer Eletronics', brand_name: 'Razer', registration_number: '', 
+                                  full_address: 'Wall Street, 341', city: 'New York', state: 'NY', email: 'razer@gmail.com')
+      # Act
+      result = new_supplier.valid?
+      # Assert
+      expect(result).to eq(false)
+    end
+
+    it 'returns false when full_address is empty' do
+      # Arrange
+      new_supplier = Supplier.new(corporate_name: 'Razer Eletronics', brand_name: 'Razer', registration_number: '131341', 
+                                  full_address: '', city: 'New York', state: 'NY', email: 'razer@gmail.com')
+      # Act
+      result = new_supplier.valid?
+      # Assert
+      expect(result).to eq(false)
+    end
+
+    it 'returns false when city is empty' do
+      # Arrange
+      new_supplier = Supplier.new(corporate_name: 'Razer Eletronics', brand_name: 'Razer', registration_number: '131341', 
+                                  full_address: 'Wall Street, 341', city: '', state: 'NY', email: 'razer@gmail.com')
+      # Act
+      result = new_supplier.valid?
+      # Assert
+      expect(result).to eq(false)
+    end
+    
+    it 'returns false when state is empty' do
+      # Arrange
+      new_supplier = Supplier.new(corporate_name: 'Razer Eletronics', brand_name: 'Razer', registration_number: '131341', 
+                                  full_address: 'Wall Street, 341', city: 'New York', state: '', email: 'razer@gmail.com')
+      # Act
+      result = new_supplier.valid?
+      # Assert
+      expect(result).to eq(false)
+    end
+
+    it 'returns false when email is empty' do
+      # Arrange
+      new_supplier = Supplier.new(corporate_name: 'Razer Eletronics', brand_name: 'Razer', registration_number: '131341', 
+                                  full_address: 'Wall Street, 341', city: 'New York', state: 'NY', email: '')
+      # Act
+      result = new_supplier.valid?
+      # Assert
+      expect(result).to eq(false)
+    end
+
+    it 'returns false when brand_name isnt unique' do
+      # Arrange
+      new_supplier_1 = Supplier.create!(corporate_name: 'Razer Eletronics', brand_name: 'Razer', registration_number: '131341', 
+                                    full_address: 'Wall Street, 341', city: 'New York', state: 'NY', email: 'razer@gmail.com')
+      new_supplier_2 = Supplier.new(corporate_name: 'Raser Eletronicos', brand_name: 'Razer', registration_number: '431341', 
+                                    full_address: 'China Town, 3431', city: 'New York', state: 'NY', email: 'raser@gmail.com')
+      # Act
+      result = new_supplier_2.valid?
+      # Assert
+      expect(result).to eq(false)
+    end
+
+    it 'returns false when registration_number isnt unique' do
+      # Arrange
+      new_supplier_1 = Supplier.create!(corporate_name: 'Razer Eletronics', brand_name: 'Razer', registration_number: '131341', 
+                                    full_address: 'Wall Street, 341', city: 'New York', state: 'NY', email: 'razer@gmail.com')
+      new_supplier_2 = Supplier.new(corporate_name: 'Raser Eletronicos', brand_name: 'Raser Eletronics', registration_number: '131341', 
+                                    full_address: 'China Town, 3431', city: 'New York', state: 'NY', email: 'raser@gmail.com')
+      # Act
+      result = new_supplier_2.valid?
+      # Assert
+      expect(result).to eq(false)
+    end
+  end
 end
