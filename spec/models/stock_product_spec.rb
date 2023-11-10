e8bd5d72ac314bb9ccd3a2efc4ee7e84f3687a87
Gabriel Manika Koeb <gmkoeb@gmail.com>
2023-11-10 11:12:18 -0300

    Include view for stock products inside warehouse page

--- spec/models/stock_product_spec.rb
+++ spec/models/stock_product_spec.rb
@@ -4,14 +4,14 @@ RSpec.describe StockProduct, type: :model do
   describe 'generates serial number' do
     it 'on stock product creation' do
       # Arrange
-      gabriel = User.create!(name: 'Gabriel', email: 'gabriel@gmail.com', password: 'password')
+      user = User.create!(name: 'Gabriel', email: 'gabriel@gmail.com', password: 'password')
       warehouse = Warehouse.create!(name: 'Guarulhos Aeroporto', code: 'GRU', city: 'Guarulhos', 
                                     area:60_000, address: 'Rua do aeroporto, 3102', 
                                     cep: '31412', description: 'Galpão localizado no aeroporto de Guarulhos' )
       supplier = Supplier.create!(corporate_name: 'Razer Eletronics', brand_name: 'Razer', registration_number: '131341', 
                                   full_address: 'Wall Street, 341', city: 'New York', state: 'NY', email: 'razer@gmail.com')
       product = ProductModel.create!(supplier: supplier, name: 'Cadeira Gamer', weight: 5, height: 100, width: 70, depth: 75, sku: 'CAD-GAMER-XPTO40')                     
-      order = Order.create!(user: gabriel, warehouse: warehouse, supplier: supplier, estimated_delivery_date: Date.tomorrow, status: 'pending')   
+      order = Order.create!(user: user, warehouse: warehouse, supplier: supplier, estimated_delivery_date: Date.tomorrow, status: 'pending')   
       # Act
       stock_product = StockProduct.create!(warehouse: warehouse, order: order, product_model: product)    
       serial_number = stock_product.serial_number 
@@ -21,7 +21,7 @@ RSpec.describe StockProduct, type: :model do
 
     it 'cant be modified' do
       # Arrange
-      gabriel = User.create!(name: 'Gabriel', email: 'gabriel@gmail.com', password: 'password')
+      user = User.create!(name: 'Gabriel', email: 'gabriel@gmail.com', password: 'password')
       warehouse = Warehouse.create!(name: 'Guarulhos Aeroporto', code: 'GRU', city: 'Guarulhos', 
                                     area:60_000, address: 'Rua do aeroporto, 3102', 
                                     cep: '31412', description: 'Galpão localizado no aeroporto de Guarulhos' )
@@ -29,7 +29,7 @@ RSpec.describe StockProduct, type: :model do
                                   full_address: 'Wall Street, 341', city: 'New York', state: 'NY', email: 'razer@gmail.com')
       product_1 = ProductModel.create!(supplier: supplier, name: 'Cadeira Gamer', weight: 5, height: 100, width: 70, depth: 75, sku: 'CAD-GAMER-XPTO40')
       product_2 = ProductModel.create!(supplier: supplier, name: 'Teclado Gamer', weight: 1, height: 3, width: 40, depth: 15, sku: 'TECLADO-GAMER-XPTO40')                     
-      order = Order.create!(user: gabriel, warehouse: warehouse, supplier: supplier, estimated_delivery_date: Date.tomorrow, status: 'pending')   
+      order = Order.create!(user: user, warehouse: warehouse, supplier: supplier, estimated_delivery_date: Date.tomorrow, status: 'pending')   
       stock_product = StockProduct.create!(warehouse: warehouse, order: order, product_model: product_1)    
       original_serial_number = stock_product.serial_number 
       # Act
---
+++ spec/system/product_stock/user_view_stock_from_warehouse_spec.rb
@@ -0,0 +1,31 @@
+require 'rails_helper'
+
+describe 'User sees stock' do
+  it 'in warehouse page' do
+    # Arrange
+    user = User.create!(name: 'Gabriel', email: 'gabriel@gmail.com', password: 'password')
+    warehouse = Warehouse.create!(name: 'Guarulhos Aeroporto', code: 'GRU', city: 'Guarulhos', 
+                                  area:60_000, address: 'Rua do aeroporto, 3102', 
+                                  cep: '31412', description: 'Galpão localizado no aeroporto de Guarulhos' )
+    supplier = Supplier.create!(corporate_name: 'Razer Eletronics', brand_name: 'Razer', registration_number: '131341', 
+                                full_address: 'Wall Street, 341', city: 'New York', state: 'NY', email: 'razer@gmail.com')
+    product_1 = ProductModel.create!(supplier: supplier, name: 'Cadeira Gamer', 
+                                     weight: 5, height: 100, width: 70, depth: 75, sku: 'CAD-GAMER-RZER404')
+    product_2 = ProductModel.create!(supplier: supplier, name: 'Teclado Gamer', 
+                                     weight: 2, height: 3, width: 40, depth: 15, sku: 'TECLADO-GAMER-RZER404')            
+    product_3 = ProductModel.create!(supplier: supplier, name: 'Mouse Gamer', 
+                                     weight: 1, height: 3, width: 7, depth: 12, sku: 'MOUSE-GAMER-RZER404')              
+    order = Order.create!(user: user, warehouse: warehouse, supplier: supplier, estimated_delivery_date: Date.tomorrow, status: 'pending')   
+    3.times { StockProduct.create!(warehouse: warehouse, order: order, product_model: product_1) }
+    2.times { StockProduct.create!(warehouse: warehouse, order: order, product_model: product_2) }
+    # Act
+    login_as(user)
+    visit root_path                               
+    click_on 'Guarulhos Aeroporto'
+    # Assert
+    expect(page).to have_content 'Itens em Estoque'
+    expect(page).to have_content '3 x CAD-GAMER-RZER404'
+    expect(page).to have_content '2 x TECLADO-GAMER-RZER404'
+    expect(page).not_to have_content 'MOUSE-GAMER-RZER404'
+  end
+end
